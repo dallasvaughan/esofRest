b9f025f25228fb48d8e4a496834817bdc2382498
Joey Miller <miller_joseph3@bah.com>
2017-02-16 11:56:04 -0500

    Removed signature2 and oracleDB

--- SignatureTests.js
+++ SignatureTests.js
@@ -4,95 +4,150 @@ chai.use(chaiHttp);
 var should = chai.should();
 var oracledb = require('oracledb');
 var server = 'http://localhost:8080';
-var response;
-var parseRes;
+var uuid;
 
 describe('GET /esof/api/v1.0/system/signatures/{status}?userId={user}', function() {
-    describe('Test Case 1: {status} = empty, {user} = user', function () {
-        getSignatureByStatus('', 'user');
-        it('Returns active and inactive signatures', function () {
-            response.should.have.status(200);
-            if (parseRes.length === 0) {
-                console.log("No signatures on file.");
-            } else {
-                parseRes.length.should.be.at.least(1);
-                console.log(parseRes.length + ' signature(s) on file:');
-                showSigsAndStatus(parseRes);
-            }
+    describe('Test Case 1: /esof/api/v1.0/system/signatures/?userId=user', function () {
+        it('Returns status 200', function(done) {
+            chai.request(server)
+                .get('/esof/api/v1.0/system/signatures/?userId=user')
+                .auth('system', 'password')
+                .end(function (err, res) {
+                    res.should.have.status(200);
+                    if (JSON.parse(res.text).length === 0) {
+                        console.log("No signatures on file.")
+                    } else {
+                        JSON.parse(res.text).length.should.be.at.least(1);
+                    }
+                    done();
+                });
         });
     });
 
-    describe('Test Case 2: {status} = active, {user} = user', function () {
-        getSignatureByStatus('active', 'user');
-        it('Returns active signatures', function () {
-            response.should.have.status(200);
-            if (parseRes.length === 0) {
-                console.log("No active signatures on file.");
-            } else {
-                parseRes.length.should.equal(1);
-                console.log(parseRes.length + ' active signature on file:');
-                showSigsAndStatus(parseRes);
-            }
+    describe('Test Case 2: /esof/api/v1.0/system/signatures/active?userId=user', function () {
+        it('Returns status 200', function(done) {
+            chai.request(server)
+                .get('/esof/api/v1.0/system/signatures/active?userId=user')
+                .auth('system', 'password')
+                .end(function (err, res) {
+                    res.should.have.status(200);
+                    if (JSON.parse(res.text).length === 0) {
+                        console.log("No signatures on file.")
+                    } else {
+                        JSON.parse(res.text).length.should.equal(1);
+                    }
+                    done();
+                });
         });
     });
 
-    describe('Test Case 3: {status} = inactive, {user} = user', function () {
-        getSignatureByStatus('inactive', 'user');
-        it('Returns inactive signatures', function () {
-            response.should.have.status(200);
-            if (parseRes.length === 0) {
-                console.log("No inactive signatures on file.");
-            } else {
-                parseRes.length.should.be.at.least(1);
-                console.log(parseRes.length + ' inactive signature(s) on file:');
-                showSigsAndStatus(parseRes);
-            }
+    describe('Test Case 3: /esof/api/v1.0/system/signatures/inactive?userId=user', function () {
+        it('Returns status 200', function(done) {
+            chai.request(server)
+                .get('/esof/api/v1.0/system/signatures/inactive?userId=user')
+                .auth('system', 'password')
+                .end(function (err, res) {
+                    res.should.have.status(200);
+                    if (JSON.parse(res.text).length === 0) {
+                        console.log("No signatures on file.")
+                    } else {
+                        JSON.parse(res.text).length.should.be.at.least(1);
+                    }
+                    done();
+                });
         });
     });
 
-    describe('Test Case 4: {status} = invalidstatus, {user} = user', function() {
-        getSignatureByStatus('invalidstatus', 'user');
-        it('Returns 404 not found', function () {
-            response.should.have.status(404);
+    describe('Test Case 4: /esof/api/v1.0/system/signatures/invalidStatus?userId=user', function () {
+        it('Returns status 404', function(done) {
+            chai.request(server)
+                .get('/esof/api/v1.0/system/signatures/invalidStatus?userId=user')
+                .auth('system', 'password')
+                .end(function (err, res) {
+                    res.should.have.status(404);
+                    done();
+                });
         });
     });
 
-    describe('Test Case 5: {status} = empty, {user} = invaliduser', function() {
-        getSignatureByStatus('', 'invaliduser');
-        it('Returns 200 empty response body', function () {
-            response.should.have.status(200);
-            parseRes.should.be.empty;
+    describe('Test Case 5: /esof/api/v1.0/system/signatures/?userId=invalidUser', function () {
+        it('Returns status 200, empty response body', function(done) {
+            chai.request(server)
+                .get('/esof/api/v1.0/system/signatures/?userId=invalidUser')
+                .auth('system', 'password')
+                .end(function (err, res) {
+                    res.should.have.status(200);
+                    JSON.parse(res.text).should.be.empty;
+                    done();
+                });
         });
     });
 
-    describe('Test Case 6: {status} = active, {user} = empty', function() {
-        getSignatureByStatus('active', '');
-        it('Returns 200 empty response body', function () {
-            response.should.have.status(200);
-            parseRes.should.be.empty;
+    describe('Test Case 6: /esof/api/v1.0/system/signatures/active?userId=', function () {
+        it('Returns status 200, empty response body', function(done) {
+            chai.request(server)
+                .get('/esof/api/v1.0/system/signatures/active?userId=')
+                .auth('system', 'password')
+                .end(function (err, res) {
+                    res.should.have.status(200);
+                    JSON.parse(res.text).should.be.empty;
+                    done();
+                });
         });
     });
 });
 
-function getSignatureByStatus(filter, user) {
-    before(function(done) {
-        chai.request(server)
-            .get('/esof/api/v1.0/system/signatures/'+filter+'?userId='+user)
-            .auth('system', 'password')
-            .end(function (err, res) {
-                response = res;
-                parseRes = JSON.parse(res.text);
-                done();
+describe('GET /esof/api/v1.0/system/signatures/{UUID}?userId={user}', function() {
+    describe('Test Case 1: /esof/api/v1.0/system/signatures/{userValidUUID}?userId=user', function () {
+        getSigUUIDFromDB('user');
+        it('Response has status 200', function(done) {
+            chai.request(server)
+                .get('/esof/api/v1.0/system/signatures/'+uuid+'?userId=user')
+                .auth('system','password')
+                .end(function(err, res) {
+                    console.log(res);
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+    });
+});
+
+function getSigUUIDFromDB(user) {
+    before(function (done) {
+        oracledb.getConnection(
+            {
+                user: "esofds",
+                password: "esofdspassword",
+                connectString: "local.db.des.usps.com:1521/XE"
+            },
+            function (err, connection) {
+                if (err) {
+                    console.error(err.message);
+                    return;
+                }
+                connection.execute(
+                    "SELECT SIGNATURE_IDENTIFIER " +
+                    "FROM SIGNATURE " +
+                    "WHERE USER_ID = '" + user + "' AND STATUS = 'ACTIVE'",
+                    function (err, result) {
+                        if (err) {
+                            console.error(err.message);
+                            doRelease(connection);
+                            return;
+                        }
+                        doRelease(connection);
+                        uuid = result.rows[0][0];
+                        done();
+                    }
+                )
             });
     });
 }
-
-function showSigsAndStatus(JSON) {
-    for (let i = 0; i < JSON.length; i++) {
-        if (JSON[i].status === 'ACTIVE') {
-            console.log('  ACTIVE signature UUID = ' + JSON[i].signatureIdentifier);
-        } else if (JSON[i].status === 'INACTIVE') {
-            console.log('INACTIVE signature UUID = ' + JSON[i].signatureIdentifier);
-        }
-    }
+function doRelease(connection) {
+    connection.close(
+        function(err) {
+            if (err)
+                console.error(err.message);
+        });
 }
